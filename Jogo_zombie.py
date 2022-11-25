7759da1e918b748934dc2f0c5d14df37c36e7e9e
Lari4174 <116530633+Lari4174@users.noreply.github.com>
2022-11-25 10:03:22 -0300

    Update Jogo_zombie.py

--- Jogo_zombie.py
+++ Jogo_zombie.py
@@ -22,13 +22,14 @@ for i in range(numJogadores): #Aqui é a nomeclatura dos jogadores, onde começa
 dadoVerde = "CPCTPC";
 dadoAmarelo = "TPCTPC";
 dadoVermelho = "TPTCPT";
+
 #Definido as variáveis que cada face do dado terá
 listaDados = [
 					dadoVerde,dadoVerde,dadoVerde,dadoVerde,dadoVerde,dadoVerde,
 					dadoAmarelo, dadoAmarelo, dadoAmarelo, dadoAmarelo,
 					dadoVermelho,dadoVermelho,dadoVermelho
 	];
-#e aqui a quantidade de dados verdes, amarelos e vermelhos do jogo
+
 print("INICIANDO O JOGO...");
 scoreJogador = [];
 jogadorAtual = 0;
@@ -36,25 +37,23 @@ dadosSorteados = [];
 tiros = 0;
 cerebros = 0;
 passos = 0;
-
-#aqui é setado as variáveis de pontuação de jogador
-while True: #vai se repetir até ser forçadamente parado
+pontua = 0;
+listaGa = [];
+while True: 
   print("TURNO DO JOGADOR ", listaJogadores[jogadorAtual]);
-  for i in range(0,3,1): #vai setar uma variável i, que começa em 1 e vai até 3 com o passo de 1 em 1, serve para sortear os dados do jogo
-    numSorteado = random.randint(0, 12); #irá randomizar um dado a ser jogado e armazenará o valor no numdado
-    dadoSorteado = listaDados[numSorteado]; # irá pegar da lista de dados e atriburi a vairável dadoSorteado
+  for i in range(0,3,1):
+    numSorteado = random.randint(0, 12); 
+    dadoSorteado = listaDados[numSorteado]; 
     if dadoSorteado == "CPCTPC":
       corDado = "VERDE";
     elif dadoSorteado == "TPCTPC":
       corDado = "AMARELO";
     else:
       corDado = "VERMELHO";
-    #teste condicional para verificar qual cor do dado
     print("Dado sorteado: ", corDado);
     dadosSorteados.append(dadoSorteado);
-    #irá adicionar o dado sorteado à lista de dadosSorteados
   print("As faces sorteadas foram: ")
-  for dadoSorteado in dadosSorteados: #fará uma lista dos dados 
+  for dadoSorteado in dadosSorteados: 
     numFaceDado = random.randint(0, 5);
     if dadoSorteado[numFaceDado] == "C":
       print("- CEREBRO (voce comeu um cerebro)");
@@ -65,56 +64,75 @@ while True: #vai se repetir até ser forçadamente parado
     else:
       print("- PASSOS (uma vitima escapou)");
       passos = passos + 1;
-    #teste condicional para verificar em qual caractere caiu
   print("SCORE ATUAL: ");
   print("CEREBROS: ", cerebros);
   print("TIROS: ", tiros);
+  VerificadorT = True;
   if tiros >= 3:
     print("AVISO: você perdeu seu score");
-    jogadorAtual = jogadorAtual + 1;
-    if jogadorAtual == len(listaJogadores):
-      jogadorAtual = 0;
+    dadosSorteados = [];
+    tiros = 0;
+    cerebros = 0;
+    passos = 0;
+    if jogadorAtual == len(listaJogadores) - 1:
+      jogadorAtual = 0; 
+      continuarJogando = input("AVISO: todos os jogadores querem jogar mais uma rodada? (s=sim / n=nao)");
+      if continuarJogando == 's':
+        continue;
+      else:
+        VerificadorT = False;
+    else:
+      jogadorAtual = jogadorAtual + 1;
+    #####Precisa de um verificador para passar, talvez um if len para pular essa parte da repetição e ir para o próximo código####
+  else:
+    VerificadorV = cerebros + score[jogadorAtual];
+    if VerificadorV >= 16:
+      print("Você atingiu o placar para ganhar, com: " + str(VerificadorV)); 
+      listaGa.append(jogadorAtual);
+      
+      #colocar um verificador para ver se a pessoa não vai ser adicionada duas vezes, o que é bem fácil eu acho... achei errado#
+    continuarTurno = input("AVISO: Voce deseja continuar jogando dados? (s=sim / n=nao)");
+    if continuarTurno == 'n':
+      pontua = cerebros;
+      Tpontua = score[jogadorAtual] + pontua;
+      score[jogadorAtual] = Tpontua;
+      print("sua pontuação atual é: " + str(score[jogadorAtual]));
+      jogadorAtual = jogadorAtual + 1;
       dadosSorteados = [];
       tiros = 0;
       cerebros = 0;
       passos = 0;
-      pass;
-    #e se for o último jogador ein ein ein ein?
-    dadosSorteados = [];
-    tiros = 0;
-    cerebros = 0;
-    passos = 0;
-    continue;
-    #####Precisa de um verificador para passar, talvez um if len para pular essa parte da repetição e ir para o próximo código####
-  continuarTurno = input("AVISO: Voce deseja continuar jogando dados? (s=sim / n=nao)");
-  if continuarTurno == 'n':
-    pontua = cerebros;
-    Tpontua = score[jogadorAtual] + pontua;
-    score[jogadorAtual] = Tpontua;
-    jogadorAtual = jogadorAtual + 1;
-    dadosSorteados = [];
-    tiros = 0;
-    cerebros = 0;
-    passos = 0;
-    print(score);
-    if jogadorAtual == len(listaJogadores):
-      continuarJogando = input("AVISO: todos os jogadores querem jogar mais uma rodada? (s=sim / n=nao)");
-      #colocar um for para a lista de jogadores imprimindo o score de cada um
-      if (continuarJogando == 's'):
-        print("Iniciando mais um turno");
-        print(jogadorAtual);
-        jogadorAtual = 0;
-        continue;
+      pontua = 0;
+  if VerificadorT == False:
+    print("Finalizando prototipo do jogo...");
+    break; 
+  else:  
+    
+    
+##acho que preciso mudar essa parte para uma separada onde as variáveis se encaixam de uma forma mais decente
+    #colocar um if com o VerificadorV para poder ver se alguém passou da pontuação de marco, se mais de uma pessoa passar, será feito o algorítimo para um comparativo talvez?
+      if jogadorAtual == len(listaJogadores):
+        continuarJogando = input("AVISO: todos os jogadores querem jogar mais uma rodada? (s=sim / n=nao)");
+        if (continuarJogando == 's'):
+          print("Iniciando mais um turno");
+          print(jogadorAtual);
+          jogadorAtual = 0;
+          continue;
+        else:
+          print("Finalizando prototipo do jogo...");
+          break;        
       else:
-        print("Finalizando prototipo do jogo...");
-        break;
-	#a partir do momento que todos os jogadores encerrarem o seu turno, o jogo encerra
-  else:
-    print("Iniciando mais uma rodada do turno atual...");
-    dadosSorteados = [];
+        print("Iniciando mais uma rodada do turno atual...");
+      dadosSorteados = [];
 
+
+jogadorAtual = 0;
+for i in range (numJogadores):
+  print("o jogador: " + str(listaJogadores[jogadorAtual]) + " fez esta pontuação: " +  str(score[jogadorAtual]));  
+  jogadorAtual = jogadorAtual + 1;
 ##  O QUE FAZER?  ##
 # verificador de pontuação, quando atingir uma certa quantidade, criar um alerta sobre e que o jogo irá encerrar
 # criar uma opção para ver os dados que estão no copo
-# criar uma forma de que os dados não jogados sejam re-rolados
+# criar uma forma de que os dados que cairam passos sejam re-rolados
 # utilizar a tupla para mostrar o resultado final, fazendo a junção de nome de jogador e o score.
+print(listaGa);
